0fb45d547af21d2a0eae0864f643ba2a9d4db6cd
sofianicora9 <sofianicora9@gmail.com>
2026-07-16 14:35:24 -0300

    nuevos disenhos del administrador

---
+++ frontend/public/js/add_mantenimiento.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("atras").addEventListener("click", function (e) {
+        e.preventDefault();
+        window.location.href = "list_mantenimientos.html";
+    });
+})
